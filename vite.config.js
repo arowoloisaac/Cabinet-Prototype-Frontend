f4346d6d768218603f2b95fd4ab6bf8abe496533
arowoloisaac <arowoloisaacabiodun01@gmail.com>
2024-11-16 14:27:22 +0700

    Template for registration and login

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const path = require("path");
+
 // https://vite.dev/config/
 export default defineConfig({
+  root: path.resolve(__dirname, "src"),
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    },
+  },
+});
